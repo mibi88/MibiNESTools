d723a935d7146e5d9e07fd5c337d468c5bfe27c9
mibi88 <mbcontact50@gmail.com>
2024-06-08 10:58:18 +0200

    Fix

--- src/main/java/io/github/mibi88/mibinestools/TilePickerTools.java
+++ src/main/java/io/github/mibi88/mibinestools/TilePickerTools.java
@@ -1,6 +1,19 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * MibiNESTools - Create NES games easily!
+ * Copyright (C) 2024  Mibi88
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 package io.github.mibi88.mibinestools;
 
